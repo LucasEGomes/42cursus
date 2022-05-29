26696d1cdfa8fde109d458b460670523585d794c
Lucas E Gomes <lucasedugomes@gmail.com>
2022-05-29 20:47:56 -0300

    Create test to bonus part. Update Makefile to include test to bonus part.

---
+++ GetNextLine/tests/bonus.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bonus.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luceduar <luceduar@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/29 00:14:29 by luceduar          #+#    #+#             */
+/*   Updated: 2022/05/29 20:21:29 by luceduar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bonus.h"
+
+int	main(int argc, char **argv)
+{
+	int	file_descriptor_0;
+	int	file_descriptor_1;
+	int	logger;
+	char	**expected_0;
+	char	**expected_1;
+	int		line_0;
+	int		line_1;
+	char	*value;
+	int		failed;
+
+	(void) argc;
+	file_descriptor_0 = open(argv[2], O_RDONLY);
+	file_descriptor_1 = open(argv[4], O_RDONLY);
+	logger = open(argv[1], O_RDWR | O_CREAT | O_TRUNC, 0644);
+	expected_0 = get_lines(argv[3]);
+	expected_1 = get_lines(argv[5]);
+	line_0 = 0;
+	line_1 = 0;
+	failed = 0;
+	printf("\t");
+	while (expected_0[line_0] != NULL && expected_1[line_1] != NULL)
+	{
+		if (expected_0[line_0] != NULL)
+		{
+			value = get_next_line(file_descriptor_0);
+			// if (value == NULL)
+			// {
+			// 	dprintf(logger, 
+			// 		"SKIP: Failed to alloc memory at `get_next_line`.\n");
+			// 	close(file_descriptor_0);
+			// 	close(logger);
+			// 	print_message(argv[0], -1);
+			// 	return (0);
+			// }
+			failed += check_content(value, expected_0[line_0], line_0 + 1, logger);
+			free(value);
+			line_0++;
+		}
+		if (expected_1[line_1] != NULL)
+		{
+			value = get_next_line(file_descriptor_1);
+			// if (value == NULL)
+			// {
+			// 	dprintf(logger, 
+			// 		"SKIP: Failed to alloc memory at `get_next_line`.\n");
+			// 	close(file_descriptor_1);
+			// 	close(logger);
+			// 	print_message(argv[0], -1);
+			// 	return (0);
+			// }
+			failed += check_content(value, expected_1[line_1], line_1 + 1, logger);
+			free(value);
+			line_1++;
+		}
+	}
+	failed += check_end_of_file(file_descriptor_0, logger);
+	failed += check_end_of_file(file_descriptor_1, logger);
+	print_message(argv[0], failed);
+	close(file_descriptor_0);
+	close(file_descriptor_1);
+	close(logger);
+	free_lines(expected_0);
+	free_lines(expected_1);
+	return (EXIT_SUCCESS);
+}
---
+++ GetNextLine/tests/bonus.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bonus.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luceduar <luceduar@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/12 12:23:52 by luceduar          #+#    #+#             */
+/*   Updated: 2022/05/29 01:03:55 by luceduar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MANDATORY_H
+# define MANDATORY_H
+# include "src/get_next_line_bonus.h"
+# include "libft/libft.h"
+# include "colors.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 256
+# endif
+# define TEST_TEMP_FILE "test_temp"
+
+int		check_line_length(char *value, char *expected, ssize_t line, int logger);
+int		check_content(char *value, char *expected, ssize_t line, int logger);
+int		check_end_of_file(int file_descriptor, int logger);
+int		check_all_content(int file_descriptor, char **expected, int logger);
+int		test_whole_file(int file_descriptor, char **expected, int logger);
+int		test_read_standard_input(char *filename, char **expected, int logger);
+
+void	*free_lines(char **lines);
+char	**get_lines(char *text);
+void	print_message(char *filename, int result);
+#endif
